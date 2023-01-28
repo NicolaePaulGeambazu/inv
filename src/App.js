84fc981e61069d7527b132d654d64205f9e556ea
Paul Geambazu <paul.geambazu@kingsbridge.co.uk>
2023-01-28 00:50:17 +0000

    first coo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import image from '../src/img/image.svg';
+import dateEvent from '../src/img/date.svg'
+import "typeface-calligraffitti"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div style={{ backgroundImage:`url(${image})`,backgroundRepeat:"no-repeat",backgroundSize:"contain", height:812,width:'100%' }}>
+    <div style={{margin: 'auto', width: '80%', paddingTop:'60%'}}>
+      <div style={{ width:'60%', textAlign: 'center', margin: 'auto', fontFamily: 'Roboto',  paddingBottom: '10px'}}>
+      You are warmly invited to the wedding of
+    </div>
+    <div style={{ width: '64%', textAlign: 'center', margin: 'auto'}}>
+    <div style={{fontFamily: 'Calligraffitti'}}>
+      Paul Nicolae Geambazu
+    </div>
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', fontFamily: 'Calligraffitti'}}>&</div>
+    <div style={{fontFamily: 'Calligraffitti'}}>Camelia Geambazu</div>
+    <div>
+      Impreuna cu  nasii nostri
+    </div>
+    <div style={{  paddingBottom: '10px'}}>
+     <div>
+      Catalin  Jiga
+    </div>
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center'}}>&</div>
+    <div>Roxana Jiga</div>
+      
+    </div>
+    </div> 
+    <div style={{  paddingBottom: '10px', marginLeft: '-25px'}}>
+      <img src={dateEvent} alt="" />
+    </div>
+    <div style={{ margin: 'auto', textAlign: 'center'}}>
+      <div>
+      La Nasu
+      </div>
+      <div style={{ width: '40%', margin: 'auto'}}>166 Tolladine Rd, Worcester</div>
+      </div> 
+    </div>
     </div>
   );
 }
